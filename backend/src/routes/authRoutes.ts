01c299524f070a7b306fb3c277a96830ff5df02d
Rishi Raj <rishiraj1632@gmail.com>
2026-01-03 14:55:40 +0530

    feat: add profile route with authentication middleware to retrieve user profile information

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { signup, login } from "../controllers/authController";
+import { signup, login, getProfile } from "../controllers/authController";
+import { authenticate } from "../middlewares/authMiddelwares";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/profile", authenticate, getProfile);
 
 export default router;
